d4a695959b0e4fba16f196e11ad3e7c082e7fb12
Franlop24 <franlopbri@gmail.com>
2024-06-11 16:02:08 -0600

    06 Implement signals in Group

--- siiuttlax/apps/group/admin.py
+++ siiuttlax/apps/group/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Group
 
-admin.site.register(Group)
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['semester', 'group', 'period', 'career']
+    fields = ['semester', 'group', 'period', 'career']
--- siiuttlax/apps/group/models.py
+++ siiuttlax/apps/group/models.py
@@ -1,25 +1,62 @@
 from django.db import models
+from django.db.models.signals import post_save
 
 from apps.period.models import Period, Semester
-from apps.career.models import Career
+from apps.career.models import Career, Subject
 from apps.academy.models import Professor
+from django.dispatch import receiver
 
 # Create your models here.
 class Group(models.Model):
     GROUPS = [('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'), ('F', 'F'), ('G', 'G'), ('H', 'H')]
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, default=1)
-    group = models.CharField(max_length=1, choices=GROUPS, default='A')
-    observations = models.CharField(max_length=50, verbose_name='Observaciones', null=True, blank=True)
-    period = models.ForeignKey(Period, on_delete=models.CASCADE)
-    career = models.ForeignKey(Career, on_delete=models.CASCADE)
-    tutor = models.ForeignKey(Professor, on_delete=models.CASCADE, null=True, blank=True)
+
+    semester = models.ForeignKey(
+        Semester, 
+        on_delete=models.CASCADE, 
+        default=1, 
+        verbose_name='Cuatrimestre')
+    group = models.CharField(
+        max_length=1, 
+        choices=GROUPS, 
+        default='A',
+        verbose_name='Grupo')
+    observations = models.CharField(
+        max_length=50, 
+        verbose_name='Observaciones', 
+        null=True, blank=True)
+    period = models.ForeignKey(
+        Period, 
+        on_delete=models.CASCADE,
+        verbose_name='Periodo')
+    career = models.ForeignKey(
+        Career, 
+        on_delete=models.CASCADE,
+        verbose_name='Carrera')
+    tutor = models.ForeignKey(
+        Professor, 
+        on_delete=models.CASCADE, 
+        verbose_name='Tutor',
+        null=True, blank=True)
+    subjects = models.ManyToManyField(
+        Subject,
+        verbose_name='Materias',
+        blank=True)
     ## Many to Many with DetailsGroup
     ## Many to Many with Students
 
+    def set_subjects(self):
+        subjects_by_career_and_semester = Subject.objects.filter(career=self.career, semester=self.semester)
+        self.subjects.set(subjects_by_career_and_semester)
+
     def __str__(self):
-        return f"{ self.semester }º{ self.group } { self.career }"
+        return f"{ self.semester } { self.group } { self.career }"
     
     class Meta:
         verbose_name = 'Grupo'
         verbose_name_plural = 'Grupos'
         ordering = ['group']
+
+@receiver(post_save, sender=Group)
+def set_subjects_signal(sender, instance, created, **kwargs):
+    if created:
+        instance.set_subjects()
