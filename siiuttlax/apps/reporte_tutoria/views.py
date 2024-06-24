4da063739f084aff3c183a3609af567bf5669c56
unknown <jersainmarin20679@gmail.com>
2024-06-24 15:07:09 -0600

    01 Estructura de reporte_tutorias.html

--- siiuttlax/apps/reporte_tutoria/views.py
+++ siiuttlax/apps/reporte_tutoria/views.py
@@ -1,3 +1,16 @@
+from datetime import date
+from urllib import request
 from django.shortcuts import render
 
-# Create your views here.
+from siiuttlax.apps import career, group
+from siiuttlax.apps.period.models import Semester
+
+def reporte_tutoria (request):context={
+    'reporte':{
+        date.fecha(''),
+        career.carrera(''),
+        Semester.grado(''),
+        group.grupo(''),
+    }
+} 
+render(request, 'reporte_tutoria.html', )
