4e004ab079f2ce3dce3e3c98c3da6a5bb879976e
ifranco07 <i.francomtz07@gmail.com>
2024-06-26 11:16:53 -0600

    reporte_tutoria 5

--- siiuttlax/apps/reporte_tutoria/models.py
+++ siiuttlax/apps/reporte_tutoria/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Tutor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    career = models.CharField(max_length=100)
+    grade = models.CharField(max_length=10)
+    group = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Activity(models.Model):
+    name = models.CharField(max_length=100)
+    objective = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class TutoringSession(models.Model):
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
+    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"Tutoring session on {self.date} by {self.tutor}"
