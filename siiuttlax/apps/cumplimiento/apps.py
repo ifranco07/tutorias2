c44298ad993b6224ba630bbcfa527f1e1bf156a5
Franlop24 <franlopbri@gmail.com>
2024-07-02 14:23:48 -0600

    fix cumplimiento integration

--- siiuttlax/apps/cumplimiento/apps.py
+++ siiuttlax/apps/cumplimiento/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CumplimientoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app.cumplimiento'
+    name = 'apps.cumplimiento'
