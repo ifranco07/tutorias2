dd31b5d1de3d082493fdf26275de2c818f907984
ifranco07 <i.francomtz07@gmail.com>
2024-07-18 13:10:05 -0600

    views&models reporte_tutorias

--- siiuttlax/apps/reporte_tutoria/models.py
+++ siiuttlax/apps/reporte_tutoria/models.py
@@ -1,37 +1,25 @@
 from django.db import models
-from django.utils import timezone
+from apps.career.models import Career
+from apps.period.models import Semester
+from apps.group.models import Group
+from apps.academy.models import Professor
 
-class Tutor(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    department = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    career = models.CharField(max_length=100)
-    grade = models.CharField(max_length=10)
-    group = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
-
-class Activity(models.Model):
-    name = models.CharField(max_length=100)
-    objective = models.TextField()
-    description = models.TextField()
+class ReporteTutoria(models.Model):
+    fecha_tutoria = models.DateField(verbose_name='Fecha de Tutoría')
+    carrera = models.ForeignKey(Career, on_delete=models.CASCADE, verbose_name='Carrera')
+    semestre = models.ForeignKey(Semester, on_delete=models.CASCADE, verbose_name='Semestre')
+    grupo = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name='Grupo')
+    nombre_actividad = models.CharField(max_length=255, verbose_name='Nombre de la Actividad')
+    objetivo_actividad = models.TextField(verbose_name='Objetivo de la Actividad')
+    descripcion_actividad = models.TextField(verbose_name='Descripción de la Actividad')
+    evidencia1 = models.FileField(upload_to='evidencias/', verbose_name='Evidencia 1', blank=True, null=True)
+    evidencia2 = models.FileField(upload_to='evidencias/', verbose_name='Evidencia 2', blank=True, null=True)
+    tutor = models.ForeignKey(Professor, on_delete=models.CASCADE, verbose_name='Tutor')
 
     def __str__(self):
-        return self.name
+        return f'Reporte de Tutoría {self.fecha_tutoria} - {self.tutor}'
 
-class TutoringSession(models.Model):
-    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
-    students = models.ManyToManyField(Student)
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
-    date = models.DateField(default=timezone.now)
-
-    def __str__(self):
-        return f"Tutoring session on {self.date} by {self.tutor}"
+    class Meta:
+        verbose_name = 'Reporte de Tutoría'
+        verbose_name_plural = 'Reportes de Tutorías'
+        ordering = ['-fecha_tutoria']
