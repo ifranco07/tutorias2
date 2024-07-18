20c751b0f61f506ce91ce44d251c834d5d524b52
ifranco07 <i.francomtz07@gmail.com>
2024-07-18 14:33:26 -0600

    Views reporte_tutorias

--- siiuttlax/apps/reporte_tutoria/views.py
+++ siiuttlax/apps/reporte_tutoria/views.py
@@ -16,8 +16,8 @@ def reporte_tutoria(request):
         nombre_actividad = request.POST.get('nombre_actividad')
         objetivo_actividad = request.POST.get('objetivo_actividad')
         descripcion_actividad = request.POST.get('descripcion_actividad')
-        evidencia1 = request.FILES.get('evidencia1')
-        evidencia2 = request.FILES.get('evidencia2')
+        evidencias = request.FILES.get('evidencias')
+        lista = request.FILES.get('lista_asistencia')
 
         reporte = ReporteTutoria(
             fecha_tutoria=fecha_tutoria,
@@ -27,15 +27,15 @@ def reporte_tutoria(request):
             nombre_actividad=nombre_actividad,
             objetivo_actividad=objetivo_actividad,
             descripcion_actividad=descripcion_actividad,
-            evidencia1=evidencia1,
-            evidencia2=evidencia2,
+            evidencias=evidencias,
+            lista=lista_asitencia,
             tutor=request.user.professor
         )
         reporte.save()
 
         
         # Redirigir a una página de éxito o a donde necesites después de guardar
-        return redirect('ruta_de_exito')  # Reemplazar 'ruta_de_exito' con la URL a donde deseas redirigir
+        return redirect('')  # Reemplazar 'ruta_de_exito' con la URL a donde deseas redirigir
 
     else:
         tutor = request.user.professor  
