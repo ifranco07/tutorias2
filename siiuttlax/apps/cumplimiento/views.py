d6787d2d856a9395db457d61e3665b68b5865710
GusDamian <damiangustavoangel@gmail.com>
2024-07-25 09:23:53 -0600

    Agregando consulta al tutor, grupo, periodo

--- siiuttlax/apps/cumplimiento/views.py
+++ siiuttlax/apps/cumplimiento/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from apps.period.models import Period
 from apps.group.models import Group
+from apps.academy.models import Professor
 
 def index(request):
     periods = Period.objects.all()
@@ -10,5 +11,23 @@ def index(request):
 def consultas_por_periodo(request, periodo_id):
     periodo = get_object_or_404(Period, id=periodo_id)
     grupos = Group.objects.filter(period=periodo)
-    grupos_data = [{'Cuatrimestre': grupo.semester.semester_name, 'Grupo': grupo.group, 'Periodo': str(grupo.period)} for grupo in grupos]
+    grupos_data = []
+    for grupo in grupos:
+        grupo_data = {
+            'Cuatrimestre': grupo.semester.semester_name,
+            'Grupo': grupo.group,
+            'Periodo': str(grupo.period),
+            'Tutor': grupo.tutor.full_name if grupo.tutor else 'No asignado'
+        }
+        grupos_data.append(grupo_data)
     return render(request, 'cumplimiento/consultas.html', {'grupos': grupos_data, 'periodo': periodo})
+
+# def consultas_por_periodo(request, periodo_id):
+#     periodo = get_object_or_404(Period, id=periodo_id)
+#     grupos = Group.objects.filter(period=periodo)
+#     grupos_data = [{'Cuatrimestre': grupo.semester.semester_name, 'Grupo': grupo.group, 'Periodo': str(grupo.period)} for grupo in grupos]
+#     return render(request, 'cumplimiento/consultas.html', {'grupos': grupos_data, 'periodo': periodo})
+
+def Tutor(request):
+    tutors = Professor.object.all()
+    return render(request, 'consultas.html', {'tutors': tutors})
