9c64f430f74cd4a13c9b4bb832b7498b946407f6
unknown <jersainmarin20679@gmail.com>
2024-07-28 22:16:14 -0600

    Save Reporte de Tutoria on database

--- siiuttlax/apps/reporte_tutoria/urls.py
+++ siiuttlax/apps/reporte_tutoria/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "reporte_tutoria"
 urlpatterns = [
     path('', views.reporte_tutoria, name='reporte_tutorias'),
+    path('success/', views.success_view, name='success'),
 ]
