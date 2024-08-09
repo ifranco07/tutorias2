6b0fb4feca7d5e8aea23b654d8c98cecb12e7f83
GusDamian <damiangustavoangel@gmail.com>
2024-08-09 12:22:30 -0600

    Adding Read Reporte_tutoria

--- siiuttlax/apps/cumplimiento/views.py
+++ siiuttlax/apps/cumplimiento/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from apps.period.models import Period
 from apps.group.models import Group
+from apps.reporte_tutoria.models import ReporteTutoria
 from django.contrib.auth.decorators import login_required
+
 
 
 @login_required
@@ -24,14 +26,24 @@ def index(request):
 def consultas_por_periodo(request, periodo_id):
     period = get_object_or_404(Period, id=periodo_id)
     grupos = Group.objects.filter(period=period)
-    grupos_data = [
-        {
+    grupos_data = []
+
+    num_actividades_esperadas = 4  # Asumiendo que se esperan 4 actividades
+
+    for grupo in grupos:
+        actividades = ReporteTutoria.objects.filter(grupo=grupo)
+        actividades_nombres = [actividad.nombre_actividad for actividad in actividades] if actividades.exists() else []
+
+        # Asegurar que siempre hay 4 actividades listadas, completar con 'No reportado'
+        actividades_completas = actividades_nombres + ['No reportado'] * (num_actividades_esperadas - len(actividades_nombres))
+
+        grupos_data.append({
             'Cuatrimestre': grupo.semester.semester_name,
             'Grupo': grupo.group,
             'Tutor': grupo.tutor.full_name if grupo.tutor else 'No asignado',
             'Periodo': str(grupo.period),
-        }
-        for grupo in grupos
-    ]
+            'Actividades': actividades_completas
+        })
+
     periods = Period.objects.all()
     return render(request, 'cumplimiento/consultas.html', {'grupos': grupos_data, 'current_period': period, 'periods': periods})
