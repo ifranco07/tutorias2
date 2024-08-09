f124b081107a0a8e7a915746838328e7c12b52a1
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2024-08-09 15:16:54 -0600

    Filter by month and career

--- siiuttlax/apps/cumplimiento/views.py
+++ siiuttlax/apps/cumplimiento/views.py
@@ -4,12 +4,10 @@ from apps.group.models import Group
 from apps.reporte_tutoria.models import ReporteTutoria
 from django.contrib.auth.decorators import login_required
 
-
-
 @login_required
 def index(request):
     periods = Period.objects.all()
-    current_period = periods.first()  
+    current_period = periods.first()
     grupos = Group.objects.filter(period=current_period)
     grupos_data = [
         {
@@ -26,24 +24,32 @@ def index(request):
 def consultas_por_periodo(request, periodo_id):
     period = get_object_or_404(Period, id=periodo_id)
     grupos = Group.objects.filter(period=period)
+    
+    # Obtener el mes del parámetro GET, si existe
+    mes = request.GET.get('mes', None)
+    
     grupos_data = []
-
-    num_actividades_esperadas = 4  # Asumiendo que se esperan 4 actividades
-
+    num_actividades_esperadas = 4
+    
     for grupo in grupos:
+        # Filtrar reportes por mes y verificar si hay reportes para el grupo
+        if mes:
+            actividades = ReporteTutoria.objects.filter(grupo=grupo, fecha_tutoria__month=mes)
+            if not actividades.exists():
+                continue  # Si no hay reportes en el mes, pasar al siguiente grupo
+        
         actividades = ReporteTutoria.objects.filter(grupo=grupo)
         actividades_nombres = [actividad.nombre_actividad for actividad in actividades] if actividades.exists() else []
-
-        # Asegurar que siempre hay 4 actividades listadas, completar con 'No reportado'
         actividades_completas = actividades_nombres + ['No reportado'] * (num_actividades_esperadas - len(actividades_nombres))
-
+        
         grupos_data.append({
             'Cuatrimestre': grupo.semester.semester_name,
             'Grupo': grupo.group,
             'Tutor': grupo.tutor.full_name if grupo.tutor else 'No asignado',
             'Periodo': str(grupo.period),
+            'Carrera': grupo.career.short_name if grupo.career else 'No asignada',
             'Actividades': actividades_completas
         })
 
     periods = Period.objects.all()
-    return render(request, 'cumplimiento/consultas.html', {'grupos': grupos_data, 'current_period': period, 'periods': periods})
+    return render(request, 'cumplimiento/consultas.html', {'grupos': grupos_data, 'current_period': period, 'periods': periods, 'selected_mes': mes})
