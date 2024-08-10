03b5992c361683a2a8983c67ab9958e4549b39ea
unknown <jersainmarin20679@gmail.com>
2024-08-10 16:29:45 -0600

    Styles reporte_tutoria and mis_reportes

--- siiuttlax/apps/reporte_tutoria/urls.py
+++ siiuttlax/apps/reporte_tutoria/urls.py
@@ -5,5 +5,5 @@ app_name = "reporte_tutoria"
 urlpatterns = [
     path('', views.reporte_tutoria, name='reporte_tutorias'),
     path('success/', views.success_view, name='success'),
-    path('mis-reportes/', views.mis_reportes, name='mis_reportes'),
+    path('mis_reportes', views.mis_reportes, name='mis_reportes'),
 ]
